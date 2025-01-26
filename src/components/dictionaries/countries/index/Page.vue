0974d0792b18f764084d2bfbaba2f75affcbdb54
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2025-01-26 23:18:22 +0100

    * Implemented success and error alert and logic on modify country function

--- src/components/dictionaries/countries/index/Page.vue
+++ src/components/dictionaries/countries/index/Page.vue
@@ -1,14 +1,16 @@
 <template>
+  <SuccessAlert :text="successMessage" v-on:alert-expired="() => {this.successMessage = undefined;}"/>
+  <ErrorAlert :text="errorMessage" v-on:alert-expired="() => {this.errorMessage = undefined;}" />
   <MainNavigationComponent ref="navBar"/>
   <div class="container">
-    <CountriesTable v-on:view="handleView" v-on:modify="handleModify" />
+    <CountriesTable ref="countriesTableComponent" v-on:view="handleView" v-on:modify="handleModify" />
   </div>
   <ModifyCountryModal
       :is-visible="modifyCountryModalVisible"
       :country="countryForModification"
       v-on:update:is-visible="(data) => { this.modifyCountryModalVisible = data; }"
-      v-on:success="(response) => {console.log(response);}"
-      v-on:error="(error) => {console.log(error);}"
+      v-on:success="handleModificationSuccess"
+      v-on:error="handleModificationError"
   />
 </template>
 
@@ -16,10 +18,14 @@
   import CountriesTable from "@/components/dictionaries/countries/index/CountriesTable.vue";
   import ModifyCountryModal from "@/components/dictionaries/countries/index/ModifyCountryModal.vue";
   import MainNavigationComponent from "@/components/partials/navigation/MainNavigationComponent.vue";
+  import SuccessAlert from "@/components/partials/alerts/SuccessAlert.vue";
+  import ErrorAlert from "@/components/partials/alerts/ErrorAlert.vue";
 
   export default {
     name: "CountriesIndexPageComponent",
     components: {
+      ErrorAlert,
+      SuccessAlert,
       MainNavigationComponent,
       CountriesTable,
       ModifyCountryModal
@@ -28,6 +34,8 @@
       return {
         countryForModification: {},
         modifyCountryModalVisible: false,
+        successMessage: undefined,
+        errorMessage: undefined,
       };
     },
     methods: {
@@ -37,6 +45,13 @@
       handleModify(country) {
         this.countryForModification = {...country};
         this.modifyCountryModalVisible = true;
+      },
+      handleModificationSuccess() {
+        this.$refs.countriesTableComponent.getAllCountries();
+        this.successMessage = `Successfully changed country data.`;
+      },
+      handleModificationError() {
+        this.errorMessage = 'Something went wrong, please try again or contact support.';
       },
     },
   }
