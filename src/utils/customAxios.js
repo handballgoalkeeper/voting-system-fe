bba69deb2210c29d35c175f36d363948fa6d56b2
Dimitrije Jelic <dimitrije.jelic00@gmail.com>
2025-02-05 01:21:47 +0100

    * Moved base backend url to env file

--- src/utils/customAxios.js
+++ src/utils/customAxios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const customAxios = axios.create({
-    // TODO: Move it to .env ...
-    baseURL: 'http://127.0.0.1:8000/api/v1.0',
+    baseURL: process.env.VUE_APP_BACKEND_URL,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
